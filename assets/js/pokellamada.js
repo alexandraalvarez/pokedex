50e27cfd2cc748fe3ae4a53d783fe47aee42e0c4
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2020-08-30 23:55:47 -0400

    iterando sobre types

--- assets/js/pokellamada.js
+++ assets/js/pokellamada.js
@@ -4,19 +4,33 @@ $(function () {
     });
 });    
 
-/*function displayCard(personaje){
+function displayCard(poke){
+  var pokeTypes = poke.types;
+  console.log(pokeTypes);
+  
+ 
+  var typesObj = [];
+
+  var types;
+  for (types in pokeTypes) {
+    typesObj.push(pokeTypes[types].type);
+  }
+   
+  console.log(typesObj);
+
+
     var card = `<div class="col-3">
                   <div class="card text-center my-4">
-                    <h5 class="card-header">${personaje.name}</h5>
-                    <img src="${personaje.image}" class="card-img-top img-fluid" alt="imagen del pokemon">
+                    <h5 class="card-header">${poke.name}</h5>
+                    <img src="${poke.sprites.front_default}" class="card-img-top img-fluid" alt="imagen del pokemon">
                     <div class="card-body">
-                      <h6 class="card-subtitle mb-2 text-muted">N° ${personaje.id}</h6>
-                      <p class="card-text"><span class="badge badge-secondary">${personaje.species}</span></p>
+                      <h6 class="card-subtitle mb-2 text-muted">N° ${poke.id}</h6>
+                      <p class="card-text"><span class="badge badge-secondary"></span></p>
                     </div>
                   </div>
                 </div>`
     return card;
-}*/
+}
 
 function getPoke(id){
     $.ajax({
@@ -25,7 +39,7 @@ function getPoke(id){
       success: function (dataPoke) {
         console.log("data=>", dataPoke);
         
-     // $("#div-insertPokes").append(displayCard(dataPoke));
+     $("#div-insertPokes").append(displayCard(dataPoke));
       }
     });
 }
